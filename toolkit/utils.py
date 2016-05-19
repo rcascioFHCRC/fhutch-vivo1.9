5e0cb0eb5d54ab71f597acf8576934d5eb5eb1fb
Ted Lawless <lawlesst@gmail.com>
2016-05-19 09:35:29 -0400

    Utility class for running a threaded harvest.

---
+++ toolkit/utils.py
@@ -0,0 +1,83 @@
+import logging
+import os
+import sys
+from Queue import Queue
+from threading import Thread
+
+from converis import client
+
+def chunk_pages(max):
+    chunk_size = 100
+    for x in range(1, max, chunk_size):
+        yield (x, (x + chunk_size) - 1)
+
+
+class ThreadedHarvest(object):
+
+    def __init__(self, q, vmodel, named_graph, threads=5):
+        self.query = q
+        self.graph = Graph()
+        self.threads = threads
+        self.vmodel = None
+
+    def get_total(self):
+        rsp = client.EntityFilter(self.query, start=0, stop=1)
+        total = rsp.number
+        logging.info("Total objects found: {}.".format(total))
+        return total
+
+    def get_pages(self):
+        mx = self.get_total()
+        out = []
+        for start, stop in chunk_pages(mx):
+            out.append((start, stop))
+        return out
+
+
+    def process(self, pair):
+        start, stop = pair
+        rsp = client.EntityFilter(self.query, start=start, stop=stop)
+        for pub in rsp:
+            self.graph += client.to_graph(pub, self.vmodel)
+
+
+    def harvest_service(self, num, harvest_q):
+        """thread worker function"""
+        while True:
+            cid = harvest_q.get()
+            logging.info('Worker: %s. Set: %s' % (num, cid))
+            value = self.process(cid)
+            harvest_q.task_done()
+        return
+
+
+    def run_harvest(self):
+        num_fetch_threads = self.threads
+        logging.info("Threaded harvest with {} threads.".format(self.threads))
+        harvest_queue = Queue()
+        # Set up some threads to fetch the enclosures
+        for i in range(num_fetch_threads):
+            worker = Thread(target=self.harvest_service, args=(i, harvest_queue,))
+            worker.setDaemon(True)
+            worker.start()
+
+        pages = self.get_pages()
+        for st_sp in pages:
+            harvest_queue.put(st_sp)
+
+        logging.debug('Harvest initialized')
+        harvest_queue.join()
+        logging.info("Threads complete.")
+
+
+    def post_updates(self, named_graph):
+        if named_graph is None:
+            raise Exception("No named graph provided")
+        logging.info("Posting updates with {} triples.".format(len(self.graph)))
+        backend.post_updates(named_graph, self.graph)
+
+    def sync_updates(self, named_graph):
+        if named_graph is None:
+            raise Exception("No named graph provided")
+        logging.info("Syncing updates with {} triples.".format(len(self.graph)))
+        backend.sync_updates(named_graph, self.graph)
