0501a581b10b2581d7dada13c040d3d8e0f9dc69
Ted Lawless <ted.lawless@thomsonreuters.com>
2016-05-20 09:51:27 -0400

    Add threaded journals and org harvest.

---
+++ toolkit/journals.py
@@ -0,0 +1,60 @@
+import os
+import logging
+import logging.handlers
+
+from converis import backend
+from converis import client
+from converis.namespaces import D, VIVO, CONVERIS
+
+# local models
+import models
+import log_setup
+
+from utils import ThreadedHarvest
+
+from rdflib import Graph, Literal
+
+logger = log_setup.get_logger()
+
+if os.environ.get('HTTP_CACHE') == "1":
+  import requests_cache
+  requests_cache.install_cache(
+     'converis',
+     backend='redis',
+     allowable_methods=('GET', 'PUT'))
+
+
+journals_q = """
+<data xmlns="http://converis/ns/webservice">
+ <query>
+  <filter for="Journal" xmlns="http://converis/ns/filterengine" xmlns:sort="http://converis/ns/sortingengine">
+  <and>
+    <and>
+     <relation minCount="1" name="PUBL_has_JOUR"/>
+    </and>
+  </and>
+  </filter>
+ </query>
+</data>
+"""
+
+class JournalHarvest(ThreadedHarvest):
+
+    def __init__(self, q, vmodel, threads=5):
+        self.query = q
+        self.graph = Graph()
+        self.threads = threads
+        self.vmodel = vmodel
+
+
+def harvest():
+    ng = "http://localhost/data/journals"
+    jh = JournalHarvest(journals_q, models.Journal)
+    jh.run_harvest()
+    logger.info("Harvest finished. Syncing to vstore.")
+    jh.sync_updates(ng)
+
+
+if __name__ == "__main__":
+    logger.info("Starting harvest.")
+    harvest()
---
+++ toolkit/organizations.py
@@ -0,0 +1,58 @@
+"""
+Harvest orgs
+"""
+import os
+import logging
+import logging.handlers
+
+# local models
+import models
+import log_setup
+
+from utils import ThreadedHarvest
+
+from rdflib import Graph
+
+logger = log_setup.get_logger()
+
+if os.environ.get('HTTP_CACHE') == "1":
+  import requests_cache
+  requests_cache.install_cache(
+     'converis',
+     backend='redis',
+     allowable_methods=('GET', 'PUT'))
+
+
+internal = """
+<data xmlns="http://converis/ns/webservice">
+ <query>
+  <filter for="Organisation" xmlns="http://converis/ns/filterengine" xmlns:sort="http://converis/ns/sortingengine">
+   <and>
+    <attribute argument="12000" name="intOrExt" operator="equals"/>
+   </and>
+  </filter>
+ </query>
+</data>
+"""
+
+class OrgaHarvest(ThreadedHarvest):
+
+    def __init__(self, q, vmodel, threads=5):
+        self.query = q
+        self.graph = Graph()
+        self.threads = threads
+        self.vmodel = vmodel
+        self.page_size = 50
+
+
+def harvest():
+    ng = "http://localhost/data/orgs"
+    jh = OrgaHarvest(internal, models.Organization)
+    jh.run_harvest()
+    logger.info("Harvest finished. Syncing to vstore.")
+    jh.sync_updates(ng)
+
+
+if __name__ == "__main__":
+    logger.info("Starting harvest.")
+    harvest()
