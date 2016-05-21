5181e663194f391b7ff5a97793811259e75813ea
Ted Lawless <lawlesst@gmail.com>
2016-05-21 11:27:22 -0400

    Basic working expertise treemap.

---
+++ app/webapp/src/main/java/org/fredhutch/controller/ExpertiseBrowseController.java
@@ -0,0 +1,108 @@
+package org.fredhutch.controller;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import com.google.gson.Gson;
+import com.hp.hpl.jena.query.*;
+import com.hp.hpl.jena.rdf.model.*;
+import edu.cornell.mannlib.vitro.webapp.controller.VitroRequest;
+import edu.cornell.mannlib.vitro.webapp.controller.freemarker.FreemarkerHttpServlet;
+import edu.cornell.mannlib.vitro.webapp.controller.freemarker.UrlBuilder;
+import edu.cornell.mannlib.vitro.webapp.controller.freemarker.responsevalues.ResponseValues;
+import edu.cornell.mannlib.vitro.webapp.controller.freemarker.responsevalues.TemplateResponseValues;
+import edu.cornell.mannlib.vitro.webapp.rdfservice.RDFServiceException;
+import edu.cornell.mannlib.vitro.webapp.rdfservice.ResultSetConsumer;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * Created by ted on 5/21/16.
+ */
+public class ExpertiseBrowseController extends FreemarkerHttpServlet {
+    private static final Log log = LogFactory.getLog(ExpertiseBrowseController.class);
+    private static final String TEMPLATE = "expertise-browse.ftl";
+    private static final String QUERY = "expertiseBrowse/getAreas.rq";
+
+    @Override
+    protected ResponseValues processRequest(VitroRequest vreq) {
+        log.debug("Generating expertise browse model");
+        Map<String, Object> body = new HashMap<String, Object>();
+        body.put("title", "Expertise");
+        JSONArray areas = getAreas(vreq);
+        body.put("areas", areas);
+        return new TemplateResponseValues(TEMPLATE, body);
+    }
+
+    public static JSONArray getAreas(VitroRequest vreq) {
+        //final ArrayList<HashMap> expertise = new ArrayList<HashMap>();
+        final JSONArray expertise = new JSONArray();
+        String rq = readQuery(QUERY);
+        ParameterizedSparqlString q2 = new ParameterizedSparqlString();
+        q2.setCommandText(rq);
+        q2.setNsPrefix("foaf", "http://xmlns.com/foaf/0.1/");
+        q2.setNsPrefix("vivo", "http://vivoweb.org/ontology/core#");
+        q2.setNsPrefix("skos", "http://www.w3.org/2004/02/skos/core#");
+        q2.setNsPrefix("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+        q2.setNsPrefix("rdfs", "http://www.w3.org/2000/01/rdf-schema#");
+        String query = q2.toString();
+        log.debug("Expertise query:\n" + query);
+        try {
+            vreq.getRDFService().sparqlSelectQuery(query, new ResultSetConsumer() {
+                @Override
+                protected void processQuerySolution(QuerySolution qs) {
+                    try {
+                        expertise.put(prepResult(qs));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+        } catch (RDFServiceException e) {
+            e.printStackTrace();
+        }
+        return expertise;
+    }
+
+    private static JSONObject prepResult(QuerySolution qs) throws JSONException {
+        JSONObject thisArea = new JSONObject();
+        Literal name = qs.getLiteral("label");
+        Resource top = qs.getResource("top");
+        Resource uri = qs.getResource("ex");
+        String group;
+        if (top.getLocalName().toString().equals("c3360927")) {
+            group = "Research and Clinical Topics";
+        } else {
+            group = "Disciplines";
+        }
+        Object number = qs.getLiteral("total").getValue();
+        thisArea.put("name", name);
+        thisArea.put("uri", uri);
+        //thisArea.put("url", getURL(uri.toString(), vreq));
+        thisArea.put("group", group);
+        thisArea.put("researchers", number);
+        return thisArea;
+    }
+
+    public static String readQuery( String name ) {
+        URL qurl = Resources.getResource(name);
+        try {
+            return Resources.toString(qurl, Charsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    public static String getURL( String uri, VitroRequest vreq ) {
+        return UrlBuilder.getIndividualProfileUrl(uri, vreq);
+    }
+}
---
+++ app/webapp/src/main/webapp/themes/fredhutch/templates/expertise-browse.ftl
@@ -0,0 +1,73 @@
+
+<h2>Explore areas of expertise</h2>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
+<!-- load D3js -->
+<script src="http://www.d3plus.org/js/d3.js"></script>
+
+<!-- load D3plus after D3js -->
+<script src="http://www.d3plus.org/js/d3plus.js"></script>
+
+
+<style>
+h1 {
+  margin-left: auto;
+  margin-right:auto;
+  width: 25%;
+  margin-bottom: 1em;
+}
+#viz {
+  width: 75%;
+  margin-left:auto;
+  margin-right:auto;
+  height: 800px;
+  margin-bottom: 4em;
+}
+
+a#view {
+    background-color: #123054;
+    color: white;
+    display: block;
+    font-size: 1.25em;
+    margin: 2em;
+    padding: .5em;
+    text-align: center;
+    text-decoration: none;
+  }
+
+</style>
+
+
+<!-- create container element for visualization -->
+<div id="viz">
+</div>
+
+<script>
+  // sample data array
+  var expertiseData = ${areas};
+
+  function tip(point) {
+    console.debug(point);
+    var vivo = "http://localhost:8080/vivo/display/"
+    var index = _.indexOf(_.pluck(expertiseData, 'name'), point);
+    var meta = expertiseData[index];
+    console.debug(meta);
+    var url = vivo + meta.uri.split('/').slice(-1)[0]
+    return "<a href=\"" + url + "\" id=\"view\">View researchers</a></h1>";
+  }
+
+  // instantiate d3plus
+  var visualization = d3plus.viz()
+    .container("#viz")  // container DIV to hold the visualization
+    .data(expertiseData)  // data to use with the visualization
+    .type("tree_map")   // visualization type
+    .id(["group", "name"])
+    .size("researchers")      // sizing of blocks
+    .tooltip({"share": false, "html": tip, "large": 300, "stacked": true})
+    .title({
+      "sub": {
+        "font": {"size": 18, "color": "navy", "family": "\"Geogrotesque-Regular\",Arial,Helvetica,Sans-serif"}
+      }
+    })
+    .draw()             // finally, draw the visualization!
+</script>
