8b24f13592d11c3653c30b14855a48151dd0d01e
Ted Lawless <ted.lawless@thomsonreuters.com>
2016-05-26 09:43:52 -0700

    Turn down logging.

--- toolkit/publications.py
+++ toolkit/publications.py
@@ -19,7 +19,7 @@ from utils import ThreadedHarvest
 
 from rdflib import Graph, Literal
 
-logger = log_setup.get_logger(client_level=logging.DEBUG)
+logger = log_setup.get_logger()
 
 
 if os.environ.get('HTTP_CACHE') == "1":
