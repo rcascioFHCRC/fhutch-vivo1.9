f4ace0edebf0f58f739a44a9e317cbefc094ba36
Ted Lawless <lawlesst@gmail.com>
2016-05-26 11:33:35 -0400

    Areas in own script.

---
+++ toolkit/areas.py
@@ -0,0 +1,54 @@
+"""
+Harvest areas
+"""
+import os
+import logging
+import logging.handlers
+
+# local models
+import models
+import log_setup
+
+from utils import ThreadedHarvest
+
+from rdflib import Graph
+
+logger = log_setup.get_logger()
+
+if os.environ.get('HTTP_CACHE') == "1":
+  import requests_cache
+  requests_cache.install_cache(
+     'converis',
+     backend='redis',
+     allowable_methods=('GET', 'PUT'))
+
+
+query = """
+<data xmlns="http://converis/ns/webservice">
+<query>
+<filter for="Area" xmlns="http://converis/ns/filterengine" xmlns:sort="http://converis/ns/sortingengine">
+</filter>
+</query>
+</data>
+"""
+
+class AreaHarvest(ThreadedHarvest):
+
+    def __init__(self, q, vmodel, threads=5):
+        self.query = q
+        self.graph = Graph()
+        self.threads = threads
+        self.vmodel = vmodel
+
+
+def harvest():
+    ng = "http://localhost/data/areas"
+    jh = AreaHarvest(query, models.Expertise)
+    jh.run_harvest()
+    logger.info("Harvest finished. Syncing to vstore.")
+    jh.sync_updates(ng)
+
+
+if __name__ == "__main__":
+    logger.info("Starting area harvest.")
+    harvest()
