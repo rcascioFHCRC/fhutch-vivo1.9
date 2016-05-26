faec45ff2ff9b15d78a68864e24ee7417315585b
Ted Lawless <lawlesst@gmail.com>
2016-05-26 11:33:26 -0400

    Cleanup people harvest. Use fhPersonType.

--- toolkit/people.py
+++ toolkit/people.py
@@ -22,7 +22,10 @@ import log_setup
 
 from rdflib import Graph, Literal
 
-logger = log_setup.get_logger(client_level=logging.DEBUG)
+from utils import ThreadedHarvest
+
+
+logger = log_setup.get_logger()
 
 if os.environ.get('HTTP_CACHE') == "1":
   import requests_cache
@@ -43,34 +46,25 @@ def add_since(time_stamp):
     )
     return ET.tostring(df)
 
+query = """
+    <data xmlns="http://converis/ns/webservice">
+     <return>
+      <attributes/>
+     </return>
+     <query>
+      <filter for="Person" xmlns="http://converis/ns/filterengine" xmlns:sort="http://converis/ns/sortingengine">
+      </filter>
+     </query>
+    </data>
+"""
 
+class PersonHarvest(ThreadedHarvest):
 
-def chunk_pages(max):
-    chunk_size = 100
-    for x in range(1, max, chunk_size):
-        yield (x, (x + chunk_size) - 1)
-
-
-class PeopleHarvest(object):
-
-    def __init__(self, q, threads=5):
+    def __init__(self, q, vmodel, threads=5):
         self.query = q
         self.graph = Graph()
         self.threads = threads
-        self.named_graph = "http://localhost/data/people"
-
-    def get_total(self):
-        rsp = client.EntityFilter(self.query, start=0, stop=1)
-        total = rsp.number
-        logging.info("Total people found: {}.".format(total))
-        return total
-
-    def get_pages(self):
-        mx = self.get_total()
-        out = []
-        for start, stop in chunk_pages(mx):
-            out.append((start, stop))
-        return out
+        self.vmodel = vmodel
 
     def process(self, pair):
         start, stop = pair
@@ -79,90 +73,22 @@ class PeopleHarvest(object):
         rsp = client.EntityFilter(self.query, start=start, stop=stop)
         for ety in rsp:
             item = client.Entity('Person', ety.cid)
+            # FH people only
             if hasattr(item, 'fhpersontype'):
                 if item.fhpersontype['cid'] == '6019159':
                     g = client.to_graph(item, models.Person)
                     self.graph += g
-                    del g
 
 
-    def harvest_service(self, num, harvest_q):
-        """thread worker function"""
-        while True:
-            cid = harvest_q.get()
-            logger.info('Worker: %s people set: %s' % (num, cid))
-            value = self.process(cid)
-            harvest_q.task_done()
-        return
-
-
-    def run_harvest(self):
-        num_fetch_threads = self.threads
-        harvest_queue = Queue()
-        # Set up some threads to fetch the enclosures
-        for i in range(num_fetch_threads):
-            worker = Thread(target=self.harvest_service, args=(i, harvest_queue,))
-            worker.setDaemon(True)
-            worker.start()
-
-        pages = self.get_pages()
-        for st_sp in pages:
-            harvest_queue.put(st_sp)
-
-        logger.debug('Harvest initialized')
-        harvest_queue.join()
-        logger.info("Threads complete.")
-
-
-    def post_updates(self):
-        logger.info("Posting updates with {} triples.".format(len(self.graph)))
-        backend.post_updates(self.named_graph, self.graph)
-
-    def sync_updates(self):
-        logger.info("Syncing updates with {} triples.".format(len(self.graph)))
-        backend.sync_updates(self.named_graph, self.graph)
-
-
-query = """
-    <data xmlns="http://converis/ns/webservice">
-     <return>
-      <attributes/>  
-      <!--<attributes>
-       <attribute name="Short description"/>
-       <attribute name="cfFamilyNames"/>
-       <attribute name="cfFirstNames"/>
-       <attribute name="middleName"/>
-       <attribute name="email"/>
-       <attribute name="ORCID"/>
-       <attribute name="academicTitle"/>
-       <attribute name="cfResInt"/>
-      </attributes>-->
-     </return>
-     <query>
-      <filter for="Person" xmlns="http://converis/ns/filterengine" xmlns:sort="http://converis/ns/sortingengine">
-       <and>
-        <and>
-         <attribute argument="12105" name="typeOfPerson" operator="equals"/>
-        </and>
-       </and>
-      </filter>
-     </query>
-    </data>
-"""
-
-def harvest(query, sync=False):
-    ph = PeopleHarvest(query)
-    #Threaded harvest
+def harvest():
+    ng = "http://localhost/data/people"
+    ph = PersonHarvest(query, models.Person)
     ph.run_harvest()
-    sync = True
-    # Send the updates to VIVO
-    if sync is True:
-        ph.sync_updates()
-    else:
-        ph.post_updates()
+    logger.info("Harvest finished. Syncing to vstore.")
+    ph.sync_updates(ng)
 
 
 if __name__ == "__main__":
     logger.info("Starting people harvest.")
-    harvest(query)
+    harvest()
 
