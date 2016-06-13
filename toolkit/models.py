d8ff611548c1e6cc342813531ea02f321199da42
Ted Lawless <ted.lawless@thomsonreuters.com>
2016-06-13 09:30:11 -0400

    Just split videos by ; and use raw text.

--- toolkit/models.py
+++ toolkit/models.py
@@ -216,8 +216,9 @@ class Person(BaseModel):
         g = Graph()
         if hasattr(self, "embeddedvideos"):
             text = self.embeddedvideos
-            for link in re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text):
-                cl = link.rstrip('&quot;')
+            #for link in re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text):
+            for link in text.split(';'):
+                cl = link.rstrip('&quot;').strip()
                 g.add((self.uri, FHD.video, Literal(cl)))
             return g
         return g
