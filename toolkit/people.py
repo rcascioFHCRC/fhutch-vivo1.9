9b0658577e49b03ba58aa78104d7cc70b90efbc9
Ted Lawless <lawlesst@gmail.com>
2016-12-05 17:01:24 -0500

    Move URI index.

--- toolkit/people.py
+++ toolkit/people.py
@@ -88,34 +88,7 @@ def harvest():
     logger.info("Harvest finished. Syncing to vstore.")
     ph.sync_updates(ng)
 
-
-def build_short_url_index():
-    query = """
-    <data xmlns="http://converis/ns/webservice">
-     <return>
-      <attributes>
-        <attribute name="shortURL"/>
-       </attributes>
-     </return>
-     <query>
-      <filter for="Person" xmlns="http://converis/ns/filterengine" xmlns:sort="http://converis/ns/sortingengine">
-        <and>
-          <attribute operator="notequals" argument="" name="shortURL"/>
-        </and>
-      </filter>
-     </query>
-    </data>
-    """
-    d = {}
-    for item in client.filter_query(query):
-        d[item.cid] = item.shorturl
-    with open('data/people.idx', 'wb') as of:
-        pickle.dump(d, of)
-
-
-
 if __name__ == "__main__":
-    build_short_url_index()
     logger.info("Starting people harvest.")
     harvest()
 
